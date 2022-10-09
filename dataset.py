af17335a1591c84697d991801f76de854b4ebe96
Jonathan Chow <jonathanchowjh@gmail.com>
2022-10-10 03:12:26 +0800

    data code

--- dataset.py
+++ dataset.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import json
 
 class MoviesData:
   def __init__(self) -> None:
@@ -5,7 +7,19 @@ class MoviesData:
     pass
 
   def load_dataset(self):
+    df = pd.read_csv('datasets/movies/movies_metadata.csv')
+    print(df.head())
+    print(df.iloc[0]['belongs_to_collection'])
+    print(df.columns)
+    y = self.json_dict(df.iloc[0]['belongs_to_collection'])
+    print(y['name'])
     return 0
   
   def get_movies(self):
     return 0
+  
+  def json_dict(self, x):
+    x = x.replace("\'", "\"")
+    return json.loads(x)
+
+MoviesData()
