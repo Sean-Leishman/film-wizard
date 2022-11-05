b22abf3c93cbb441b47104abe17d4be756dafad0
Sean Leishman <leishmansean@gmail.com>
2022-11-05 15:56:07 +0800

    made each paragraph 500 characters in the wiki scraper

--- data_scraper/scraper.py
+++ data_scraper/scraper.py
@@ -29,20 +29,23 @@ class Driver():
         combined_paragraph = combined_paragraph.strip()
         return combined_paragraph
 
+    def split_paragraph(self,string):
+        return [string[x:x+500] for x in range(0,len(string), 500)]
+
     def get_paragraphs_by_movie_rows(self, movie_rows):
         wiki_urls = [movie_row.wikipediaUrl for movie_row in movie_rows]
         combined_paragraphs = [self.generate_paragraph(wiki_url) for wiki_url in wiki_urls]
-
-        return combined_paragraphs
+        return [self.split_paragraph(x) for x in combined_paragraphs]
 
     def get_paragraphs_by_wiki_urls(self, wiki_urls):
         combined_paragraphs = [self.generate_paragraph(wiki_url) for wiki_url in wiki_urls]
-        return combined_paragraphs
+        return [self.split_paragraph(x) for x in combined_paragraphs]
 
 if __name__ == "__main__":
     driver = Driver()
     # Toy Story
-    print(driver.get_paragraphs_by_wiki_urls(["https://en.wikipedia.org/wiki/Toy_Story"]))
+    st = driver.get_paragraphs_by_wiki_urls(["https://en.wikipedia.org/wiki/Toy_Story"])
+
     """example_urls = [862,807,11,274870,339403]
     example_paths = ["toy_story.txt", "seven.txt", "star_wars.txt", "passengers.txt", "baby_driver.txt"]
 
