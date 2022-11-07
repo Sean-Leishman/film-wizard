1892dfdb8b7da966aca3b23bacaea67e269fe1c6
Sean Leishman <leishmansean@gmail.com>
2022-11-07 10:40:26 +0800

    fix errors

--- chatbot_class.py
+++ chatbot_class.py
@@ -7,10 +7,12 @@ from transformers import pipeline
 import truecase
 import re
 from flashtext import KeywordProcessor
+from qa_class import QA_Chat
 
 """
     The purpose of this class is to build out the functions and methods so we can store the information in an easy and accessible way.
 """
+
 
 class ChatbotFramework():
 
@@ -21,6 +23,11 @@ class ChatbotFramework():
         self.chatbot_mode = chatbot_mode
 
         self.keyword_processor = KeywordProcessor()
+
+        # This is the loaded qa model
+        self.qa_model = ""
+        self.answer_list = ""
+        self.answer_state = 0
 
         # Keywords file
         keywords_json_file = open('data_cleaning/datasets/keywords.json')
@@ -45,9 +52,8 @@ class ChatbotFramework():
         self.model = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER-uncased")
         self.ner_pipeline = pipeline("ner", model=self.model, tokenizer=self.tokenizer, grouped_entities=True)
 
-        #Intent keywords list:
+        # Intent keywords list:
         self.intent = {'hello': {'hello', 'hi', 'how do you do', 'howdy', 'hullo'},
-<<<<<<< HEAD
                        'reset': {'reset', 'quit'},
                        'quit': {'cease', 'chuck up the sponge', 'depart', 'discontinue', 'drop by the wayside',
                                 'give up', 'lay off', 'quit', 'relinquish', 'renounce', 'resign', 'step down', 'stop',
@@ -58,44 +64,37 @@ class ChatbotFramework():
                                  'moving picture show', 'picture show'},
                        'qa': {"enquire", "questions", "ask", "query", "want to know", "question"}}
 
-=======
-                    'reset': {'reset', 'quit'},
-                    'quit': {'cease','chuck up the sponge', 'depart', 'discontinue', 'drop by the wayside', 'give up', 'lay off', 'quit', 'relinquish', 'renounce', 'resign', 'step down', 'stop', 'take leave', 'throw in', 'throw in the towel'},
-                    'recommend': {'advocate', 'commend', 'recommend', 'urge', 'recommended'},
-                    'no': {'none', 'nothing', 'no', 'no more', 'not interested', "i don't know"},
-                    'movie': {'film', 'flick', 'motion picture', 'motion picture show', 'movie', 'moving picture', 'moving picture show', 'picture show'}}
-        
->>>>>>> parent of f44227c (change to master without models)
         self.intent_keys = list(self.intent.keys())
 
         self.intent_response = {
             'reply_greet_general': 'Hello back to you!',
             'reply_fallback_general': "I don't quite understand you, could you clarify your question?",
             'reply_movie_recommended': "Thank you! Please go through our prompts as they appear! Type reset/quit to quit the recommender mode",
-            'reply_reset': "It appears you'll like to reset your chat, all status wiped!"
+            'reply_reset': "It appears you'll like to reset your chat, all status wiped!",
+            'qa_mode': "What movie do you want to know more about? Type reset/quit to quit the QA Mode",
         }
 
     def convo(self, input):
         ## There are three different modes, intent detection, recommender mode and qa
 
-        #What to respond back
+        # What to respond back
         response_list = []
 
         if self.chatbot_mode == 'intent_detection':
 
             input_lower = input.lower()
-            
-            convo_keyword_proc  = KeywordProcessor()
+
+            convo_keyword_proc = KeywordProcessor()
             intent_detected = []
-            
-            for intent,pattern in self.intent.items():
+
+            for intent, pattern in self.intent.items():
                 convo_keyword_proc.add_keywords_from_list(list(pattern))
 
                 if len(convo_keyword_proc.extract_keywords(input_lower)) > 0:
-                    intent_detected.append(intent) 
+                    intent_detected.append(intent)
 
                 convo_keyword_proc.remove_keywords_from_list(list(pattern))
-            
+
             if len(intent_detected) == 0:
                 return [self.intent_response['reply_fallback_general']]
 
@@ -105,24 +104,24 @@ class ChatbotFramework():
                     if intent == "hello":
                         response_list.append(self.intent_response['reply_greet_general'])
                     elif intent == "recommend":
-                        #Going into the movie pipeline
+                        # Going into the movie pipeline
                         response_list.append(self.response_generator_for_recommender('prompt_genre'))
-                        self.chatbot_mode =  'movie_recommender'
-            
-            return response_list
-            
-        elif self.chatbot_mode == 'movie_recommender':
-<<<<<<< HEAD
-            print("enter movie recommender")
-            # Check if user wants to reset
-=======
+                        self.chatbot_mode = 'movie_recommender'
+                    elif intent == "qa":
+                        # Going into the movie pipeline
+                        response_list.append(self.intent_response['qa_mode'])
+                        self.chatbot_mode = 'qa'
 
-            #Check if user wants to reset
->>>>>>> parent of f44227c (change to master without models)
+            return response_list
+
+        elif self.chatbot_mode == 'movie_recommender':
+
+            # Check if user wants to reset
             if self.reset_convo(input) == False:
                 respond_results = self.get_input_respond_message_for_movie_recommender(input)
+                self.recommended_movies = respond_results
             else:
-                #Go back to intent detection
+                # Go back to intent detection
                 self.tracker = 0
                 self.chatbot_mode = 'intent_detection'
                 return [self.intent_response['reply_reset']]
@@ -130,9 +129,76 @@ class ChatbotFramework():
             return respond_results
 
         elif self.chatbot_mode == 'qa':
-            #what to say during QA part
-            pass
-    
+            # what to say during QA part
+            # Check if user wants to reset
+            if self.reset_convo(input) == False:
+                self.qa_model = self.qa_question_chatbot(input)
+                respond_results = ["We have loaded the data for " + input]
+                respond_results.append("Please ask your question")
+                self.chatbot_mode = "qa_questions"
+
+            else:
+                # Go back to intent detection
+                self.tracker = 0
+                self.chatbot_mode = 'intent_detection'
+                return [self.intent_response['reply_reset']]
+
+            return respond_results
+
+        elif self.chatbot_mode == "qa_questions":
+
+            if self.reset_convo(input) == False:
+                self.answer_list = self.qa_model.question_answer_chatbot(input)
+                self.answer_state = 0
+                first_answer = self.qa_model.answer_generator_chatbot(self.answer_list, self.answer_state)
+                respond_results = [first_answer]
+                respond_results.append("Is this answer satisfactory? (Y/N)")
+                self.chatbot_mode = "qa_answers"
+            else:
+                # Go back to intent detection
+                self.tracker = 0
+                self.chatbot_mode = 'intent_detection'
+                return [self.intent_response['reply_reset']]
+
+            return respond_results
+
+        elif self.chatbot_mode == "qa_answers":
+
+            if self.reset_convo(input) == False:
+                # Checking the state of chatbot_answers
+                if input == "N" or input == "n":
+                    self.answer_state += 1
+                    answer = self.qa_model.answer_generator_chatbot(self.answer_list, self.answer_state)
+
+                    while answer == "":
+                        self.answer_state += 1
+                        answer = self.qa_model.answer_generator_chatbot(self.answer_list, self.answer_state)
+
+                    if answer == "Oops There is no more answer":
+                        self.answer_state = 0
+                        self.chatbot_mode = "qa_questions"
+                        respond_results = ["Please ask another question or type reset to start over!"]
+                        return respond_results
+
+                    respond_results = [answer]
+                    respond_results.append("Is this answer satisfactory? (Y/N)")
+
+                elif input == "Y" or input == "y":
+                    self.answer_state = 0
+                    self.chatbot_mode = "qa_questions"
+                    respond_results = ["Please ask another question or type reset to start over!"]
+
+                else:
+                    respond_results = ['Please answer the question!!']
+
+            else:
+                # Go back to intent detection
+                self.tracker = 0
+                self.chatbot_mode = 'intent_detection'
+                return [self.intent_response['reply_reset']]
+
+            return respond_results
+
     def reset_convo(self, input):
         if input.lower() == "quit" or input.lower() == "reset":
             return True
@@ -169,6 +235,7 @@ class ChatbotFramework():
             'reply_list_of_movies': self.recommend_movies,
             'reply_movie_prompt': "Which movie would you like to know more about?",
             'reply_end': "Thank you for using our chatbot! Type reset to reset the state of the chatbot",
+            'reply_qa': self.qa_mode,
         }
 
         return responses[response_prompt]
@@ -178,9 +245,9 @@ class ChatbotFramework():
         temp_impt_keywords = []  # This will be appended into recommender
         skip_mode = False
 
-        #Check if users wants to skip this question
-        
-        no_keyword_proc  = KeywordProcessor()
+        # Check if users wants to skip this question
+
+        no_keyword_proc = KeywordProcessor()
         no_keyword_proc.add_keywords_from_list(list(self.intent['no']))
 
         want_to_skip_intent = no_keyword_proc.extract_keywords(input)
@@ -190,12 +257,12 @@ class ChatbotFramework():
 
         # The below is the prompt respond logic
         list_of_prompt_response = [
-            #{'prompt_greet': ['reply_greet', 'prompt_genre']},
+            # {'prompt_greet': ['reply_greet', 'prompt_genre']},
             {'prompt_genre': ['reply_generic_response', 'prompt_shows']},
             {'prompt_shows': ['reply_generic_response', 'prompt_actor']},
-            {'prompt_actor': ['reply_generic_response','prompt_production_companies']},
+            {'prompt_actor': ['reply_generic_response', 'prompt_production_companies']},
             {'prompt_production_companies': ['reply_generic_response', 'prompt_themes']},
-            {'prompt_themes': ['reply_searching', 'reply_list_of_movies']},
+            {'prompt_themes': ['reply_searching', 'reply_list_of_movies', "reply_qa"]},
             {'reply_list_of_movies': ['reply_end']}
         ]
 
@@ -209,7 +276,7 @@ class ChatbotFramework():
         truecase_input = truecase.get_true_case(input)
         print(truecase_input)
 
-        #If there's no skip
+        # If there's no skip
         if skip_mode == False:
 
             if prompt_type == 'prompt_genre':
@@ -273,7 +340,7 @@ class ChatbotFramework():
         return responses_result
 
     def ner_recognition(self, input):
-        #input_rejoin = " ".join(input)
+        # input_rejoin = " ".join(input)
         ner_results = self.ner_pipeline(input)
 
         entities = self.link_related_ner_together(ner_results)
@@ -300,37 +367,32 @@ class ChatbotFramework():
     def find_words_phrases_from_keywords(self, sentence):
         keywords_found = self.keyword_processor.extract_keywords(sentence)
         return keywords_found
-    
-    def link_related_ner_together(self, ner_entities):
-            i = 0
-            keywords =  []
-            while i < len(ner_entities):
-                word = ner_entities[i]['word']
-                next_num = i+1
-                print(len(ner_entities)-1)
-                if (next_num) > (len(ner_entities) - 1):
-                    keywords.append(word)
-                    i+=1
-                else:
-                    if '#' in ner_entities[i+1]['word']:
-                        print('here')
-                        keywords.append(word + ner_entities[i+1]['word'].lstrip('#'))
-                        i+=2
-                    else:
-                        keywords.append(word)
-                        i+=1
-            return keywords
-                    
 
-<<<<<<< HEAD
+    def link_related_ner_together(self, ner_entities):
+        i = 0
+        keywords = []
+        while i < len(ner_entities):
+            word = ner_entities[i]['word']
+            next_num = i + 1
+            print(len(ner_entities) - 1)
+            if (next_num) > (len(ner_entities) - 1):
+                keywords.append(word)
+                i += 1
+            else:
+                if '#' in ner_entities[i + 1]['word']:
+                    print('here')
+                    keywords.append(word + ner_entities[i + 1]['word'].lstrip('#'))
+                    i += 2
+                else:
+                    keywords.append(word)
+                    i += 1
+        return keywords
+
     def qa_question_chatbot(self, movie_name):
         qa_model = QA_Chat("./models/distilbert-QuAC-SQUAD", movie_name)
         return qa_model
 
     def qa_mode(self):
         self.chatbot_mode = "qa"
-        return "Switching to QA Mode. Type in the name of the movie you would like to answer Questions for"
-=======
-
->>>>>>> parent of f44227c (change to master without models)
+        return "Switching to QA Mode."
 
